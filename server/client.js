e554754276f99494613972512b7c13d1ccb03ab5
Dion Steel <dion.steel@canstar.com.au>
2017-03-04 18:21:29 +1000

    Client/server classes

---
+++ server/client.js
@@ -0,0 +1,19 @@
+var server = "ws://localhost:8080";
+
+var Client = require("./classes/Client");
+var wsclient = require("ws").Client;
+var websocket = require('websocket-stream');
+var extend = require('extend')
+var duplexStream = websocket(server);
+var setup = defaultSetup;
+var client = new Client({
+  serverStream: duplexStream,
+  container: document.body,
+})
+
+// use the client.connection [Duplexconnection](https://github.com/pgte/duplex-connection) to react to remote events
+client.connection.on('join', function(user) {
+  console.log(user,'joined.')
+})
+
+console.log( client);
