f049eeb2f2518ae9530943d7a0880f0f12a515af
Heather Roberts <heather.roberts@canstar.com.au>
2017-03-05 12:47:04 +1000

    Add push notification functionality

--- Dash/app/main.ts
+++ Dash/app/main.ts
@@ -1,4 +1,28 @@
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
+import * as local from 'nativescript-local-notifications';
+
+local.addOnMessageReceivedCallback(
+  function(notificationData) {
+    console.log(JSON.stringify(notificationData));
+  }
+);
+
+setTimeout(function() {
+  local.requestPermission().then(() => {
+    local.schedule([{
+      id: 0,
+      title: 'New Transaction',
+      body: 'Dash has detected a new business transaction'
+    }]).then(() => {
+      console.log("Notification scheduled");
+    }, (error) => {
+      console.log("ERROR", error);
+    });
+  })
+
+}, 10000);
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
+
+
